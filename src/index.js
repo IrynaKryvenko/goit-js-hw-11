f13e9e245fe24554750d0c4b1aacca3c884ba29a
IrynaKryvenko <kryvenkoirina93@gmail.com>
2023-05-25 20:40:08 +0300

    upload

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import axios from 'axios';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { renderPhotosList } from './js/renderPhotosList';
-
-const API_KEY = '36655990-0724db180fb71d9be8c2c1bf3';
-const BASE_URL = 'https://pixabay.com/api/?key=';
-const OPTIONS = '}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40';
+import { getPhotos } from './js/apiService';
 
 const submitForm = document.querySelector('.search-form');
 const galleryList = document.querySelector('.gallery');
@@ -38,7 +34,7 @@ function onSearch(event) {
 
 function onLoadPhotos() {
   loadMoreBtn.style.setProperty('display', 'none', 'important');
-  getPhotos(searchQuery)
+  getPhotos(searchQuery, page)
     .then((array) => {
       renderPhotosList(array.hits, galleryList, gallery);
       total += array.hits.length;
@@ -73,24 +69,4 @@ function forScrollPage() {
     top: cardHeight * 2,
     behavior: 'smooth',
   });
-};
-
-async function getPhotos(name) {
-  try {
-    const response = await axios.get(
-      BASE_URL + API_KEY + `&q=${name}` + OPTIONS + `&page=${page}`
-    );
-    total += response.data.hits.length;
-    const totalHits = response.data.totalHits;
-    if (total >= totalHits) {
-      Notify.warning('We`re sorry, but you`ve reached the end of search results.');
-      loadMoreBtn.style.setProperty('display', 'none', 'important');
-    }
-    return response.data;
-  } catch (error) {
-    Notify.failure(
-      'Sorry, there are no images matching your search query. Please try again.'
-    );
-    throw error;
-  } 
 };
